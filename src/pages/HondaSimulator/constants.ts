54330b812d1e518556dca6ca8a12ec878514cc2a
AugustoMarcelo <mrclgst10@gmail.com>
2020-09-08 14:11:24 -0300

    :speech_balloon: added fixed values

---
+++ src/pages/HondaSimulator/constants.ts
@@ -0,0 +1,17 @@
+const Constants = {
+  ipva: {
+    cc199: 0.025,
+    cc200: 0.03,
+  },
+  seguro: {
+    normal: 0,
+    obrigatorio: 15,
+  },
+  taxa_detran: 154,
+  placa: 120,
+  despachante: 50,
+  comissao_loja: 200,
+  vistoria: 50,
+};
+
+export default Constants;
---
+++ src/pages/VWSimulator/constants.ts
@@ -0,0 +1,20 @@
+const Constants = {
+  ipva: 0.03,
+  seguro: {
+    normal: 17,
+    utilitario: 20,
+  },
+  placas: 198,
+  taxa_detran: 154,
+  despachante: 40,
+  servico_loja: 600,
+  vistoria: 50,
+
+  mudanca_municipio: 54,
+  aliencacao_fiduciaria: 54,
+  substituicao_placa: 190,
+  transferencia: 100,
+  servico_loja_usados: 250,
+};
+
+export default Constants;
